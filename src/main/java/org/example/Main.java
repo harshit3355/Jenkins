1daf2d059f8822fd0ef27fb91f0fae983c8d6d83
Harshit Nagila <harshitnagila555@gmail.com>
2024-02-20 12:34:20 +0530

    First Commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+	public static void sum(int a, int b){
+		System.out.println(a+b);
+	}
+	public static void main(String[] args)
+    {
+		System.out.println("Hello world!");
+		sum(5,10);
+	}
+}
